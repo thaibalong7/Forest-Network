98634ae54dc540166522842134cfc2b78608d098
thaibalong7 <thai.balong.7@gmail.com>
2018-12-04 09:25:55 +0700

    + design UI

---
+++ forest-network/src/containers/HomeContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Home from './../components/Home'
+class HomeContainer extends Component {
+	render() {
+		return (
+			<Home>
+                
+            </Home>
+		);
+	}
+}
+
+export default HomeContainer;
