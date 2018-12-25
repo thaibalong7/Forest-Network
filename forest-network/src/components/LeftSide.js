c19ba7287cbbfdc86514505b1dba2a8159560c64
LEHUULY-PC\LEHUULY <lehuuly1512313@gmail.com>
2018-12-25 20:59:40 +0700

    Update Change Name

--- forest-network/src/components/LeftSide.js
+++ forest-network/src/components/LeftSide.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/Home.css'
 import '../index.css'
+import '../styles/style.css'
 import { BrowserRouter as Router, Route, Link, Switch, withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
 class LeftSide extends Component {
@@ -10,8 +11,15 @@ class LeftSide extends Component {
         var Bandwidth = "Bandwidth: " + this.props.userProfileReducer.bandwidth
         return (
             <div className="w-full lg:w-1/4 pl-4 lg:pl-0 pr-6 mt-8 mb-4">
-                <h2><div className="text-black font-bold no-underline hover:underline">{this.props.userProfileReducer.name}</div></h2>
+                <div>
+                <h2><div 
+                className="text-black font-bold no-underline hover:underline"
+                data-toggle="modal"
+                data-target="#modalchangename"
+                >{this.props.userProfileReducer.name}</div></h2>
 
+
+                </div>
                 <div className="mb-4">
                     Team group of tblong, nhhung and lhly
                 </div>
@@ -34,6 +42,39 @@ class LeftSide extends Component {
                         data-target="#myModal">Add Transaction
                     </button>
                     {/* <!-- The Modal --> */}
+
+                    <div className="modal fade"
+                        id="modalchangename"
+                        data-backdrop="true"
+                        data-keyboard="true"
+                        tabIndex="1"
+                        aria-labelledby="myModalLabel"
+                        aria-hidden="true"
+                    >
+                        <div className="modal-dialog position">
+                            <div className="modal-content">
+
+                                <div className="modal-header">
+                                    <h4 className="modal-title">Update name</h4>
+                                    <button type="button" className="close" data-dismiss="modal">&times;</button>
+                                </div>
+
+                                <div className="modal-body">
+                                    <form>
+                                        <div>
+                                            <h3>name: </h3>
+                                            <input
+                                                type="text"
+                                                className="bg-grey-lighter h-8 px-4 py-2 text-xs w-48 rounded-full"
+                                                value = {this.props.userProfileReducer.name}
+                                                 />
+                                        </div>
+                                        <button type="submit" className="style" data-dismiss="modal">Change</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="modal fade"
                         id="myModal"
                         data-backdrop="true"
